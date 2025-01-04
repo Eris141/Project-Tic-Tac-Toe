23dc325e67da57c178a1a227bd08fd4718b09c2e
Eris141 <165952691+Eris141@users.noreply.github.com>
2025-01-04 19:25:04 +0100

    feat: compare the moves, win,tie, play the game at the console

--- js-code/script.js
+++ js-code/script.js
@@ -3,14 +3,15 @@
 
 const GameBoard = (function() {
   const board = [];
-  const BoardSize = 3;
+  const boardSize = 3;
   let currentPlayer = "X";
+  let gameOver = false;
   createBoard();
   
   function createBoard() {
-    for (let i = 0; i < BoardSize; i++) {
+    for (let i = 0; i < boardSize; i++) {
       board[i] = [];
-      for (let j = 0; j < BoardSize; j++) {
+      for (let j = 0; j < boardSize; j++) {
         board[i].push(Cell().getValue());
       }
     }
@@ -20,14 +21,15 @@ const GameBoard = (function() {
   board.length = 0;
   createBoard();
   currentPlayer =  "X";
+  gameOver = false;
   console.log("new game started");
   return board;
  }
 
   const getBoard = () => board;
 
-  const isValidPosition = (row, colum) => {
-    return row >= 0 && row < BoardSize && colum >= 0 && colum < BoardSize; 
+  const isValidPosition = (row, column) => {
+    return row >= 0 && row < boardSize && column >= 0 && column < boardSize; 
   }
 
   function togglePlayer() {
@@ -35,49 +37,80 @@ const GameBoard = (function() {
   }
 
 
-  function addValue(row, colum) {
+  function addValue(row, column) {
+    if (gameOver) {
+      return  `Game is over.  Please reset the board to play again.`;
+    }
 
-    if (isValidPosition(row, colum)) {
-      
-      if (board[row][colum] === ' ') {
-        board[row][colum] = currentPlayer;
+    if (isValidPosition(row, column)) {
+      if (board[row][column] === ' ') {
+        board[row][column] = currentPlayer;
         const lastPlayer = currentPlayer;
-        console.log(checkForGameOver(lastPlayer));
-        togglePlayer();
-      } else if (board[row][colum] !== ' ') {
-        board[row][colum] = currentPlayer;
-        console.log("cant mark the same row twcie")
-      } 
+        const result = checkForGameOver(lastPlayer);
 
-    } else return "Try inside the board"
+        if (result !== "Game is ongoing") {
+          console.log(result);
+        }
+
+        togglePlayer();
+      } else {
+        return "Cell is already taken";
+      } 
+      
+    } else return "Invalid position."
     
     return board;
   }
   
   function checkForGameOver(lastPlayer) {
+
+    function checkForEqualConditions(value1, value2, value3) {
+      if (value1 === value2 && value2 === value3  && value1 !== ' ') {
+        return true;
+      } else return false;
+    }
+
     for (let i = 0; i < board.length; i++ ) {
       if (
-        board[i][0] === board[i][1] && board[i][1] === board[i][2] && board[i][0] !== ' '
+        checkForEqualConditions(board[i][0], board[i][1], board[i][2]) || checkForEqualConditions(board[0][i], board[1][i], board[2][i]) 
       ) {
-        console.log('checking row',i , board[i])
-        return  `row win, winner ${lastPlayer}`;
-      } else if (board[0][i] === board[1][i] && board[1][i] === board[2][i] && board[0][i] !== ' '){
-        return  `colum winner ${lastPlayer}`;
+        gameOver = true;
+        return  `Player ${lastPlayer} Wins. Please reset to start a new game.`
       }
-    }
-    
-    if (board[0][0] === board[1][1] && board[1][1] === board[2][2] && board[0][0] !== ' ') {
-      return `diagonal win(top left to bottom right winner ${lastPlayer}`
-    }
+      // if (
+      //   board[i][0] === board[i][1] && board[i][1] === board[i][2] && board[i][0] !== ' '
+      // ) {
+      //   gameOver = true;
+      //   return  `Player ${lastPlayer} Wins. Please reset to start a new game. `;
 
-    if (board[0][2] === board[1][1] && board[1][1] === board[2][0] && board[0][2] !== ' ') {
-      return `diagonal win( top right to bottom left winner ${lastPlayer}` 
+      // } else if (board[0][i] === board[1][i] && board[1][i] === board[2][i] && board[0][i] !== ' '){
+      //   gameOver = true;
+      //   return  `Player ${lastPlayer} Wins. Please reset to start a new game. `;
+      // }
     }
+   
+    //check for diagnal win
+    if (checkForEqualConditions(board[0][0], board[1][1], board[2][2] || checkForEqualConditions(board[0][2], board[1][1], board[0][2]))) {
+      gameOver = true;
+      return `Player ${lastPlayer} Wins. Please reset to start a new game.`;
+    }
+    // if (board[0][0] === board[1][1] && board[1][1] === board[2][2] && board[0][0] !== ' ') {
+    //   gameOver = true;
+    //   return `Player ${lastPlayer} Wins. Please reset to start a new game. `;
+    // }
+
+    // if (board[0][2] === board[1][1] && board[1][1] === board[2][0] && board[0][2] !== ' ') {
+    //   gameOver = true;
+    //   return `Player ${lastPlayer} Wins. Please reset to start a new game. `; 
+    // }
 
     const isBoardFull = board.every(row => row.every(cell => cell !== ' '));
-    if (isBoardFull) return "Is' a Draw"
+    if (isBoardFull) {
+      gameOver = true;
+      return "Is' a Draw! Please reset to start a new game.";
+    } 
 
-    return "game is on"
+    return "Game is ongoing";
   }
   
 
@@ -100,19 +133,41 @@ function Cell() {
 }
 
 console.log(GameBoard.getBoard());
-console.log(GameBoard.addValue(1, 2));
+console.log(GameBoard.addValue(2, 0));
 console.log(GameBoard.addValue(2, 2));
 console.log(GameBoard.addValue(0, 0));
+console.log(GameBoard.addValue(1, 2));
 console.log(GameBoard.addValue(1, 0));
-console.log(GameBoard.addValue(2, 0));
-console.log(GameBoard.addValue(0, 2));
-console.log(GameBoard.addValue(2, 1));
-console.log(GameBoard.addValue(0, 1));
 console.log(GameBoard.addValue(1, 1));
 console.log(GameBoard.resetBoard());
-console.log(GameBoard.getBoard());
+console.log(GameBoard.addValue(2, 2));
+console.log(GameBoard.addValue(2, 1));
+console.log(GameBoard.addValue(0, 2));
+console.log(GameBoard.addValue(0, 1));
+console.log(GameBoard.addValue(2, 0));
 console.log(GameBoard.addValue(1, 1));
+console.log(GameBoard.addValue(1, 2));
+console.log(GameBoard.addValue(1, 1));
+console.log(GameBoard.resetBoard());
+console.log(GameBoard.addValue(0, 0));
+console.log(GameBoard.addValue(0, 1));
+console.log(GameBoard.addValue(0, 2));
+console.log(GameBoard.addValue(2, 2));
+console.log(GameBoard.addValue(1, 1));
+console.log(GameBoard.addValue(1, 2));
+console.log(GameBoard.addValue(2, 1));
+console.log(GameBoard.addValue(2, 0));
 console.log(GameBoard.addValue(1, 0));
+console.log(GameBoard.addValue(1, 0));
+console.log(GameBoard.resetBoard());
+console.log(GameBoard.addValue(0, 0));
+console.log(GameBoard.addValue(1, 2));
+console.log(GameBoard.addValue(1, 1));
+console.log(GameBoard.addValue(2, 1));
+console.log(GameBoard.addValue(2, 2));
+console.log(GameBoard.addValue(2, 2));
+
+
 
 
 
